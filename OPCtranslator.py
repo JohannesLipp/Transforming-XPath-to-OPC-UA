6505d4903a4af0b1f05da86de0e2c80cd307a74f
Max <64021553+Max-lab-prog@users.noreply.github.com>
2021-06-09 11:14:27 +0200

    Update OPCtranslator.py

--- OPCtranslator.py
+++ OPCtranslator.py
@@ -150,7 +150,7 @@ async def getDescendant(client, connected, nodeList, orSelf, offlineList, precfo
         a = checkSet^(returnSet.intersection(checkSet)) 
         returnSet.update(checkSet)
         checkSet = a
-
+    returnSet.update(await child(client, connected, returnSet, offlineList))
     return returnSet
 
 
